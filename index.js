5054082fb4656c021fe03c26177c40c72d84240f
Matt Briselli <briselli@adobe.com>
2018-06-21 10:40:23 -0400

    working js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const app = express()
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const { Client } = require('pg');
+const app = express();
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
 });
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -16,14 +16,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//     }
+//     client.end();
+// });
+
+
+app.get('/', (req, res) => {
+    res.render('pages/index', {"client": client});
 });
-
-
-app.get('/', (req, res) => res.render('pages/index'))
 app.get("/stock", (req, res) => res.render('pages/stock'))
