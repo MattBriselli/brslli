aea48e1446cada84b8a01965b56a0a2024a98790
Matt Briselli <briselli@adobe.com>
2018-06-21 10:41:19 -0400

    workign js

--- public/scripts/wow.js
+++ public/scripts/wow.js
@@ -1,7 +1,3 @@
-const { Client } = require('pg');
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
+$(document).on("ready", function () {
+    console.log("client");
 });
-client.connect();
-console.log(client);
