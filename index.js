a043daaf915b45a279653932e7c3e6ca2915b6c4
Matt Briselli <briselli@adobe.com>
2018-06-21 08:48:49 -0400

    web

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+
+app.get('/', (req, res) => res.render('pages/index'))
 app.get("/stock", (req, res) => res.render('pages/stock'))
