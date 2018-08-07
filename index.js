f0ebb65458bc4e95f1b23e402e41892d65bd4bc6
Matt Briselli <briselli@adobe.com>
2018-08-07 07:47:59 -0400

    reduction and simplification

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const { Client } = require('pg');
 const app = express();
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
+const firebase = require('firebase');
 
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//     if (err) throw err;
-//     for (let row of res.rows) {
-//         console.log(JSON.stringify(row));
-//     }
-//     client.end();
-// });
-
-
 app.get('/', (req, res) => {
-    res.render('pages/index', {"client": client});
+    res.render('pages/index', {});
 });
 app.get("/stock", (req, res) => res.render('pages/stock'));
 app.get("/profile", (req, res) => res.render('pages/profile'));
